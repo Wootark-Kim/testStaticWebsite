afcbba418ddcec08294e370aa7519bc36a2a28b6
Tom <167953580+Wootark-Kim@users.noreply.github.com>
2025-12-30 20:41:38 -0800

    Progress - worked on the CST338 page

--- js/include.js
+++ js/include.js
@@ -1,8 +1,32 @@
 // alert used for testing purpose
 // alert("js successfully loaded!");
 
+/*
 fetch("header.html")
 	.then(res => res.text())
 	.then(html => {
 		document.getElementById("header").innerHTML=html;
 	});
+*/
+
+//=============================================================
+// loading header partial (compatible with a static website)
+async function loadHeader(){
+	const page = await fetch("partials/header.html");
+	const res = await page.text();
+	document.getElementById("header").innerHTML=res;
+}
+loadHeader();
+//=============================================================
+
+
+//=============================================================
+// loading footer partial (compatible with a static website)
+async function loadFooter(){
+	const page = await fetch("partials/footer.html");
+	const res = await page.text();
+	document.getElementById("footer").innerHTML=res;
+}
+loadFooter();
+//=============================================================
+
